f9bbf4b5c4e5b1b4f6e5836d555acfcf2c406902
om21prakash <om21prakash@users.noreply.github.com>
2014-08-12 13:21:10 +0530

    Update CalendarPrint.py

--- CalendarPrint.py
+++ CalendarPrint.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 # Takes user input & prints calendar for that month
+# Validation includes if user input for year is 4 digits & month falls between 1 to 12
+# PS: cal function by default throws error if its our of range. I wanted to display understandable message.
 
 import calendar
+year=raw_input("Enter a year")
 
-year=raw_input("Enter a number")
-mon=raw_input("Enter a number")
-
-cal = calendar.month(int(year), int(mon))
-print "Here is the calendar:"
-print cal;
+if year.isdigit() and len(year) ==4 :
+    mon=raw_input("Enter a month")
+    if (int(mon) >=1 and int(mon) <=12):
+        cal = calendar.calendar.month(int(year), int(mon))
+        print "Here is the calendar:"
+        print cal
+    else:
+        print "value entered is out of range"
+else:
+    print "Enter a valid 4 digit integer for year"
