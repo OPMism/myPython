7ee86dd3aaf9e94764cce88b3dd82e7254af601c
om21prakash <om21prakash@users.noreply.github.com>
2014-08-12 12:28:58 +0530

    Create ListExample1.py

---
+++ ListExample1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# Demonstration of list functionality & string splicing
+
+var1 = 'Hello World!'
+var2 = "Python Practise"
+list1 = [var1, var2, 1997, 2000];
+list2 = [1, 2, 3, 4, 5, 6, 7 ];
+
+print "list1[0]: ", list1[0]
+print "list2[1:5]: ", list2[1:5]
+print "var1[0]: ", var1[0]
+print "var2[1:5]: ", var2[1:5]
