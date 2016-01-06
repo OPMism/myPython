b58e34736000e52401d1c9ae8ed12a5c1a7287cd
Om Prakash Manivannan <om21prakash@users.noreply.github.com>
2016-01-06 15:38:19 -0600

    printAcro.py
    Answer for http://sites.fas.harvard.edu/~qr20/assignments/assign2.html

---
+++ printAcro.py
@@ -0,0 +1,10 @@
+#Program to print the acronym (based upon case)
+def printAcro(x):
+        finaltext = ""
+        abbrev=[item[:1] for item in x.split()] #Loop the input string & split to words
+        for character in abbrev:        #Group only the upper case characters
+                if character.isupper():
+                        finaltext = finaltext + character       #Prepare the final string
+        print finaltext         #Print the prepared string
+        
+acronym = printAcro(raw_input("Enter an String: "))
