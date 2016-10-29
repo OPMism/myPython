14bf364537aa2b0bb7441998db2872ff57cf1802
Baheshtur <eudes.nvn@hotmail.com>
2016-10-29 13:21:36 -0300

    Fixed the "cal" variable
    calendar.calendar.month --> calendar.month

--- CalendarPrint.py
+++ CalendarPrint.py
@@ -4,15 +4,15 @@
 # PS: cal function by default throws error if its our of range. I wanted to display understandable message.
 
 import calendar
-year=raw_input("Enter a year")
+year = raw_input("Enter a year: ")
 
-if year.isdigit() and len(year) ==4 :
-    mon=raw_input("Enter a month")
+if year.isdigit() and len(year) == 4:
+    mon = raw_input("Enter a month: ")
     if (int(mon) >=1 and int(mon) <=12):
-        cal = calendar.calendar.month(int(year), int(mon))
+        cal = calendar.month(int(year), int(mon))
         print "Here is the calendar:"
         print cal
     else:
-        print "value entered is out of range"
+        print "Value entered is out of range"
 else:
     print "Enter a valid 4 digit integer for year"
